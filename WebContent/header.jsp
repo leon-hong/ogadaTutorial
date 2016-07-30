660d85169fbbd76745f9520beb5291019b089909
leonh <leonh@DESKTOP-1PCLLAT>
2016-07-31 01:12:09 +0900

    2016-07-31 JTLS 적용 및 header, footer 분리
    jstl import 이용하여 파일 분리
    jstl 적용할 파일 설정

---
+++ WebContent/header.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="main.jsp">Leon's Home</a>
+			</div>
+
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="main.jsp">Main</a></li>
+					<li><a href="jstl.jsp">JSTL</a></li>
+					<li><a href="#Session">Session</a></li>
+					<li><a href="#DB">DB</a></li>
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="index.jsp">Logout</a></li>
+					<li><a href="http://www.enjoyhongcha.com">홍내영님</a></li>
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+
+		</div>
+	</div>
+</nav>
--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -13,36 +12,37 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		request.setCharacterEncoding("UTF-8");
-		
+
 		java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
- 		String today = formatter.format(new java.util.Date());
-	
+		String today = formatter.format(new java.util.Date());
+
 		String USER_ID = "abcd";
 		String USER_PW = "1234";
-	
+
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
-		
+
 		boolean isSuccess = false;
-		if( USER_ID.equals(id) && USER_PW.equals(pw) ){
+		if (USER_ID.equals(id) && USER_PW.equals(pw)) {
 			isSuccess = true;
 		}
-		
+
 		String isLogin = "N";
 		String pathRes = "bootStrapLogin.jsp";
-		if( isSuccess ){
+		if (isSuccess) {
 			isLogin = "Y";
 			pathRes = "main.jsp";
 		}
- 		
- 		request.setAttribute("isLogin", isLogin);
- 		request.setAttribute("today", today);
- 		
- 		RequestDispatcher view = request.getRequestDispatcher(pathRes);
- 		view.forward(request, response);
+
+		request.setAttribute("isLogin", isLogin);
+		request.setAttribute("today", today);
+
+		RequestDispatcher view = request.getRequestDispatcher(pathRes);
+		view.forward(request, response);
 	}
 
 }
